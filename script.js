65b6a43b6bf7c7c92f57630c7b20b0dc14c378e7
shirasaki-h <shirasaki-h@gnavi.co.jp>
2018-02-05 15:25:51 +0900

    調整中

--- script.js
+++ script.js
@@ -4,19 +4,27 @@ const btn       = document.getElementById('js-areaModalBtn');
 const modal     = document.getElementById('js-modal');
 
 
-btn.addEventListener('click', function(){
-    const openHeight = (window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop;
-    window.pageYOffset = openHeight;
-    modal.classList.toggle('is-open');
-    // contents.classList.toggle('is-none');
-});
+btn.addEventListener(
+    'click',
+    () => {
+        const openHeight = (window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop;
+        window.pageYOffset = openHeight;
+        modal.classList.toggle('is-open');
+        // contents.classList.toggle('is-none');
+    },
+    false
+)
 
-window.addEventListener('scroll', function(){
-    let scrollTop = (window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop;
-    if(scrollTop > 110){
-        header.classList.add('is-sticky');
-    } else {
-        header.classList.remove('is-sticky');
-    }
-    console.log(scrollTop);
-})
+window.addEventListener(
+    'scroll',
+    () =>  {
+        let scrollTop = (window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop;
+        if(scrollTop > 110){
+            header.classList.add('is-sticky');
+        } else {
+            header.classList.remove('is-sticky');
+        }
+        console.log(scrollTop);
+    },
+    false
+)
